ecc31df3bcb879071e83cd8cbdefc2c2fefd6215
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-07-15 16:10:54 +0900

    環境を整えた

--- app/setting.py
+++ app/setting.py
@@ -2,9 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 # Engine の作成
-Engine = create_engine(
-  "mysql+pymysql://dbuser:password@db/mydb",
-  encoding="utf-8",
-  echo=False
-)
+Engine = create_engine("mysql+pymysql://dbuser:password@db/mydb", encoding="utf-8", echo=False)
 Base = declarative_base()
