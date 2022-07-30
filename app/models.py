72091f5c75cd140f8c397a3bdfe99f9f2c2104c1
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-07-30 23:45:10 +0900

    update:  type を変換

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData
+from sqlalchemy import Column, MetaData, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.sql.functions import random
 from sqlalchemy.sql.schema import UniqueConstraint
-from sqlalchemy.sql.sqltypes import DATETIME
+from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.types import Integer, String
 
 convention = {
@@ -21,12 +21,8 @@ _Base = declarative_base(metadata=metadata)
 
 class BaseModel(_Base):
     __abstract__ = True
-    created = Column(DATETIME, default=datetime.now)
-    modified = Column(DATETIME, default=datetime.now)
-
-    def __init__(self):
-        self.created = datetime.now
-        self.modified = datetime.now
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
     def __repr__(self) -> str:
         columns = ", ".join([f"{k}={repr(v)}" for k, v in self.__dict__.items() if not k.startswith("_")])
