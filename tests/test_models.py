b14dc4d74463a4723797ca8d33a207d98c6d2b3b
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-08-22 16:19:17 +0900

    update: test が動いた

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,10 +3,11 @@ from app import models
 
 class TestUsers:
 
-    def _create_tenant_and_users(self, session):
+    @staticmethod
+    def _create_tenant_and_users(session):
         tenant = models.Tenant(name="新撰組")
         session.add(tenant)
-        session.commit()
+        session.commit(),
         user = models.User(tenant_id=tenant.id, name="近藤勇")
         session.add(user)
         session.commit()
@@ -22,7 +23,10 @@ class TestUsers:
         assert user.tenant.name == "新撰組"
         assert tenant.users[0].id == user.id
 
-    def test_tenantを削除するとuserも削除される(self, session):
+    def test_tenantを削除するとuserも削除される( # noqa
+            self,
+            session
+        ):
         tenant, user = self._create_tenant_and_users(session)
         session.delete(tenant)
         session.commit()
